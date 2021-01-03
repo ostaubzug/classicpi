238f5fad64ef9c817ec2ef6cdbe3468a07cc89d0
ostaub <ostaubzug@gmail.com>
2021-01-03 12:54:48 +0100

    loop works if initiated

--- pythonmusicplayer.py
+++ pythonmusicplayer.py
@@ -14,26 +14,54 @@ def randomfile():
 
 
 def play():
+    global musicfile
+    
     musicfile = randomfile()
-
     mixer.music.load("/home/pi/Music/" + musicfile)
-    mixer.music.set_volume(0.5)
+    mixer.music.set_volume(0.6)
     mixer.music.play()
+    shuffle()
     
 
-def playing():
+def shuffle():
     while True:
         print("Press 'p' to pause")
         print("Press 'r' to resume")
+        print("Press 'n' for a new song")
+        print("Press 'l' to initiate loop playing")
         ch = input("['p','r']>>>")
         if ch == "p":
             mixer.music.pause()
         elif ch == "r":
             mixer.music.unpause()
+        elif ch == "n":
+            play()
+        elif ch == "l":
+            loop()
+        
+def loop():
+    while True:
+        print("Press 'p' to pause")
+        print("Press 'r' to resume")
+        print("Press 'n' for a new song")
+        print("Press 's' to initiate shuffle play ")
+        ch = input("['p','r']>>>")
+        if ch == "p":
+            mixer.music.pause()
+        elif ch == "r":
+            mixer.music.unpause()
+        elif ch == "n":
+            mixer.music.load("/home/pi/Music/" + musicfile)
+            mixer.music.set_volume(0.6)
+            mixer.music.play()
+            loop()
+
+        elif ch == "s":
+            shuffle()
 
 
 play()
-playing()
+
 
 
 # while True:
