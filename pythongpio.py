ebdc2bae2de4ca5b3119bba47c952f7704d2b717
Oliver Staub <ostaubzug@gmail.com>
2021-01-03 17:09:53 +0100

    latest commit

--- pythongpio.py
+++ pythongpio.py
@@ -1,3 +1,21 @@
 #gpio board
+import RPi.GPIO as GPIO
+import time
 
-#test
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(13, GPIO.IN)
+GPIO.setup(19, GPIO.IN)
+GPIO.setup(26, GPIO.IN)
+
+while True:
+    if GPIO.input(13) == 1:
+        print("13")
+        time.sleep(0.5)
+    if GPIO.input(19) == 1:
+        print("19")
+        time.sleep(0.5)
+    if GPIO.input(26) == 1:
+        print("26")
+        time.sleep(0.5)
+#test 13 19 26
